bd211798ea932e43dd713060b0a9d49ea63b4ecc
Lenovo <Lenovo@DESKTOP-AJ5INH1>
2022-08-24 09:57:09 +0300

    Добавьте файлы проекта.

---
+++ C SHRP DS AND CLASSWORK/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SHRP_DS_AND_CLASSWORK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //він пустий бо це проба і я дз і класні роботи буду добавляти в гілки
+        }
+    }
+}
